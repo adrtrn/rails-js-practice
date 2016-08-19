e5b3cb9d94f4a28ce6ca972ce798bfcbc109d028
olivecoil <adrian.s.tran@gmail.com>
2016-08-19 16:33:30 -0700

    passed game model and helper spec tests

--- spec_tests/spec/helpers/games_helper_spec.rb
+++ spec_tests/spec/helpers/games_helper_spec.rb
@@ -29,22 +29,66 @@ describe GamesHelper do
 
   describe "#result_for_user_throw" do
     context "when the user won the game" do
-      it "returns 'covers' when the user threw paper"
-      it "returns 'cut' when the user threw scissors"
-      it "returns 'crushes' when the user threw rock"
+      it "returns 'covers' when the user threw paper" do 
+        game = Game.new(user_throw: "paper", computer_throw: "rock")
+        expect(helper.result_for_user_throw(game)).to eq "covers"
+      end
+
+      it "returns 'cut' when the user threw scissors" do 
+        game = Game.new(user_throw: "scissors", computer_throw: "paper")
+        expect(helper.result_for_user_throw(game)).to eq "cut"
+      end
+
+      it "returns 'crushes' when the user threw rock" do 
+        game = Game.new(user_throw: "rock", computer_throw: "scissors")
+        expect(helper.result_for_user_throw(game)).to eq "crushes"
+      end
     end
 
     context "when the computer won the game" do
-      pending
+      it "returns 'is covered by' when the computer threw paper" do 
+        game = Game.new(computer_throw: "paper", user_throw: "rock")
+        expect(helper.result_for_user_throw(game)).to eq "is covered by"
+      end
+
+      it "returns 'is cut by' when the computer threw scissors" do 
+        game = Game.new(computer_throw: "scissors", user_throw: "paper")
+        expect(helper.result_for_user_throw(game)).to eq "is cut by"
+      end
+
+      it "returns 'is crushed by' when the computer threw rock" do 
+        game = Game.new(computer_throw: "rock", user_throw: "scissors")
+        expect(helper.result_for_user_throw(game)).to eq "is crushed by"
+      end
     end
   end
 
   describe "#throw_color" do
-    pending
+    it "returns danger" do 
+      expect(helper.throw_color("rock")).to eq "danger"
+    end
+
+    it "returns success" do 
+      expect(helper.throw_color("paper")).to eq "success"
+    end
+
+    it "returns info" do 
+      expect(helper.throw_color("scissors")).to eq "info"
+    end
   end
 
   describe "#throw_fa_icon" do
-    pending
+    it "returns fa hand rock o if rock" do 
+      expect(helper.throw_fa_icon("rock")).to eq "fa-hand-rock-o"
+    end
+
+    it "returns fa stop o if paper" do 
+      expect(helper.throw_fa_icon("paper")).to eq "fa-hand-stop-o"
+    end
+
+    it "returns fa-hand-scissors-o" do
+      expect(helper.throw_fa_icon("scissors")).to eq "fa-hand-scissors-o"
+    end
   end
 
 end
